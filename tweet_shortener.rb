5f9a12cc88770667b8790c9b1f78dffa6d2db4e1
Umair Qaisar <qaisaru@icloud.com>
2018-09-16 13:15:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,58 @@
 # Write your code here.
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+# You can use regex or, check out the solution below that
+# def word_substituter(tweet)
+#   tweet = tweet.dup
+#   dictionary.each do |word, replacement|
+#     if tweet.include?(word)
+#       tweet.gsub!(/\b#{word}\b/i, replacement)
+#     end
+#   end
+#   tweet
+# end
+
+
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
+  end
+end
